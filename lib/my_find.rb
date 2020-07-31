0a879eb02cee8b1db6443f9908aa6e2ecfa28baf
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-07-31 21:02:02 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -9,3 +9,13 @@ def my_find(collection)
     i = i + 1
   end
 end
+
+#these are the same, the bottom is refactored though because in Ruby you can write if unless actions in one line.
+
+def my_find(collection)
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i = i + 1
+  end
+end
